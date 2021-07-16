8539d5a4374983543a92d9f4f38f4050b0f511d1
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2021-07-16 22:56:49 +0530

    Plotting
    Using MathLab and Octave done this Plotting

---
+++ Geomatric Plotting/convex.m
@@ -0,0 +1,20 @@
+alpha=rand(1,100);
+
+b=13;
+x=0;
+convex=1;
+
+for n=1:length(alpha)
+  c=alpha(n)+x;
+  if(c>b)
+  convex=0;
+  break;
+endif
+
+endfor
+
+if(!convex)
+disp('c is bigger');
+else
+disp('c not biger');
+endif
---
+++ Geomatric Plotting/function1.m
@@ -0,0 +1,3 @@
+function [y] = func1 (x)
+  y = 1./(1 + x.^2);
+endfunction
